b1827604b4ece4831c55c43b6a0c44ed3627ca42
jeseung <testblank@naver.com>
2022-01-11 17:02:02 +0900

    commit

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src/'),
+      '@': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src/components/'),
       '@api': path.resolve(__dirname, 'src/api/'),
       '@redux': path.resolve(__dirname, 'src/redux/'),
